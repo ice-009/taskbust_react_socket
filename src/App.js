e952a45df06de58c767fb18ae5b817faa2e0de12
ice-009 <21bma009@nith.ac.in>
2024-03-14 00:44:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import io from 'socket.io-client';
 
-function App() {
+const SOCKET_SERVER_URL = 'http://localhost:8080/'; // Change this to your Socket.IO server URL
+
+const App = () => {
+  useEffect(() => {
+    // Connect to Socket.IO server
+    const socket = io(SOCKET_SERVER_URL);
+
+    try{
+    socket.on('connect', () => {
+      console.log('Connected to Socket.IO server');
+    });
+  }
+  catch(err){
+    console.log(err);
+  }
+    // Listen for custom events and log them to the console
+    socket.on('some-event', (data) => {
+      console.log('Received some-event:', data);
+    });
+
+    // Listen for 'disconnect' event
+    socket.on('disconnect', () => {
+      console.log('Disconnected from Socket.IO server');
+    });
+
+    // Clean up on unmount
+    return () => {
+      socket.disconnect();
+    };
+  }, []); // Run effect only once on component mount
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Socket.IO Console Logger</h1>
+      {/* Your component JSX */}
     </div>
   );
-}
+};
 
 export default App;
